96691b54478a43fc17d1d78120d96b9811f58f89
Stan Stevens <sstevens404@gmail.com>
2017-02-07 23:57:38 -0500

    Initial commit. Read and print out file.

--- HackAssembler/main.c
+++ HackAssembler/main.c
@@ -9,7 +9,25 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    FILE *inputFile = fopen("/Users/FireCrotch/Documents/Programming/Learning/nand2tetris/projects/06/add/Add.asm", "r");
+    
+    if (inputFile == NULL) {
+        fprintf(stderr, "Cant open input file\n");
+        return 1;
+    }
+    
+    FILE *outputFile = fopen("/Users/FireCrotch/Desktop/ouput.hack", "w");
+    
+    if (outputFile == NULL) {
+        fprintf(stderr, "Cant open ouput file\n");
+        return 1;
+    }
+    
+    int c;
+    
+    while ((c = fgetc(inputFile)) != EOF) {
+        printf("%c", c);
+    }
+    
     return 0;
 }
